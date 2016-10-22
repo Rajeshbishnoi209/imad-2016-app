38d3a67f99cde0e0a07902459f3607ad6a072ba0
Rajeshbishnoi209 <bishnoiorajesh209@gmail.com>
2016-10-23 00:37:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/', function (req, res) {
 app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/ui/bishnoi.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bishnoi.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
